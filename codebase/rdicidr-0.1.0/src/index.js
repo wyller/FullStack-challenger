bc8be7f71de0dd77d9914904b072c808d294a25c
Wyller <wyllerxd@gmail.com>
2025-08-26 11:27:09 -0300

    test pipeline

--- codebase/rdicidr-0.1.0/src/index.js
+++ codebase/rdicidr-0.1.0/src/index.js
@@ -15,4 +15,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // teste cicd
+
+const teste = () => {console.log("quebrar o prettier")};
 reportWebVitals();
